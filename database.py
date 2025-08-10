d3e6737c41f4527e90928e49be1ca2325c1be983
Caio Freitas <caio078freitas@gmail.com>
2025-08-09 23:12:41 -0300

    chore: altera os placeholders das imagens para imagens reais

--- database.py
+++ database.py
@@ -26,11 +26,11 @@ def populate_tbs(conn: sqlite3.Connection):
     if count == 0:
         print('Banco de dados vazio. Populando com dados iniciais...')
         medias = [
-            ('A Origem', 'Sci-Fi', 'Um ladrão que invade sonhos.', 'https://placehold.co/150/png'),
-            ('Interestelar', 'Sci-Fi', 'Viagem no tempo e espaço para salvar a humanidade.', 'https://placehold.co/150/png'),
-            ('Batman: O Cavaleiro das Trevas', 'Ação', 'Batman enfrenta o Coringa.', 'https://placehold.co/150/png'),
-            ('Forrest Gump', 'Drama', 'A vida de um homem simples com um grande coração.', 'https://placehold.co/150/png'),
-            ('Matrix', 'Ficção', 'Descubra a verdade sobre a realidade.', 'https://placehold.co/150/png')
+            ('A Origem', 'Sci-Fi', 'Um ladrão que invade sonhos.', 'https://m.media-amazon.com/images/S/pv-target-images/3f122417c55feda5c465f701320892661bfea27c1dfcff81e7fb0641ba29171c.jpg'),
+            ('Interestelar', 'Sci-Fi', 'Viagem no tempo e espaço para salvar a humanidade.', 'https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/aa5b9295-8f9c-44f5-809b-3f2b84badfbf/74a67d78-7c7e-47e8-9603-20fe2a00eae4?host=wbd-images.prod-vod.h264.io&partner=beamcom'),
+            ('Batman: O Cavaleiro das Trevas', 'Ação', 'Batman enfrenta o Coringa.', 'https://m.media-amazon.com/images/S/pv-target-images/49eeda38b5fe2ef033861ef6b07ea914eeef437cdb1dd35e282d9acce008779e._SX1080_FMjpg_.jpg'),
+            ('Forrest Gump', 'Drama', 'A vida de um homem simples com um grande coração.', 'https://wp-content.amenteemaravilhosa.com.br/2015/05/forrestgump.jpg'),
+            ('Matrix', 'Ficção', 'Descubra a verdade sobre a realidade.', 'https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABZzDupwylH-h0zoEyASxaxb-eXBvlskslcNE-zYTrF4-vtehLHmkb13FL95R8M9mjji5whxBux6iS-fKTRiHju_wAuMgRi7Dwybo.jpg?r=608')
         ]
         
         cursor.executemany("""
